72c7ad764b6b89969e3e2da283d595490f4dd69a
kenguru33 <bernt.anker@rs.no>
2021-11-25 00:30:17 +0100

    fixed test

--- packages/nestjs-nats-jetstream-transport-test-app/src/app.controller.spec.ts
+++ packages/nestjs-nats-jetstream-transport-test-app/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to webshop"', () => {
+      expect(appController.home()).toBe('Welcome to webshop');
     });
   });
 });
