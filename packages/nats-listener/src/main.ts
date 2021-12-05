52005c58a1a94f2c17f68c92692f5e2509789320
kenguru33 <bernt.anker@rs.no>
2021-12-05 20:38:34 +0100

    Updated demo code for auto create/update stream

--- packages/nats-listener/src/main.ts
+++ packages/nats-listener/src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomStrategy } from '@nestjs/microservices';
 import { NatsJetStreamServer } from '@nestjs-plugins/nestjs-nats-jetstream-transport';
+import { StorageType } from 'nats';
 
 async function bootstrap() {
   const options: CustomStrategy = {
@@ -18,11 +19,18 @@ async function bootstrap() {
         deliverTo: 'test-service',
         manualAck: true,
       },
+      streamConfig: {
+        name: 'my-stream',
+        subjects: ['order.*'],
+        storage: StorageType.Memory,
+      },
     }),
   };
 
   const app = await NestFactory.create(AppModule);
   const microService = app.connectMicroservice(options);
-  microService.listen();
+  await microService.listen();
 }
+
+// noinspection JSIgnoredPromiseFromCall
 bootstrap();
