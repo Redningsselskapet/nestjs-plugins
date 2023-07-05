88f6078e696d903c82b8e25d19e345869bb3e8ac
Bernt Anker <bernt.anker@me.com>
2023-07-06 00:48:26 +0200

    bump version

--- packages/nestjs-nats-jetstream-transport/src/index.ts
+++ packages/nestjs-nats-jetstream-transport/src/index.ts
@@ -9,3 +9,4 @@ export * from './interfaces/server-consumer-options.interface';
 export * from './interfaces/nats-stream-config.interface';
 export * from './utils/server-consumer-options-builder';
 export * from './nats-jetstream.context';
+// bump version
